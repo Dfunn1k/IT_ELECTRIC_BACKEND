7d18ca2b72bdb7c12bf95e737f411318d7f4445d
Dfunn1k <danychavezdev@gmail.com>
2023-06-20 18:32:40 -0500

    feat: struch finish

--- RestApi/wsgi.py
+++ RestApi/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
+print(os.getenv("INT_MYSQL_DB"))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestApi.settings')
 
